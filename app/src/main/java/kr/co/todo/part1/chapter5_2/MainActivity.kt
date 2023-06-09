048f4b2a7060ab0453fe04a9f1f84497e8caadd3
HJ <gksgpwls0306@naver.com>
2023-06-09 17:29:27 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/todo/part1/chapter5_2/MainActivity.kt
@@ -0,0 +1,217 @@
+package kr.co.todo.part1.chapter5_2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.co.todo.part1.chapter5_2.ui.theme.ViewModelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ViewModelTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TopLevel()
+                }
+            }
+        }
+    }
+}
+
+// 단계 2: `ViewModel`을 상속받은 `ToDoViewModel`을 만듭니다.
+// 첫 단계에서는 내용을 비워두고 시작합시다.
+
+// 단계 3: `TopLevel`의 파라미터로 `ToDoViewModel` 타입의
+// `viewModel`을 전달합니다. 기본 값은 `viewModel()`로 설정합시다.
+// 에러가 발생하면 아래의 `import` 문을 추가합니다.
+// `import androidx.lifecycle.viewmodel.compose.viewModel`
+@Composable
+fun TopLevel() {
+    // 단계 4: text, setText를 뷰 모델로 옮겨봅시다.
+    // 뷰 모델의 프로퍼티로 변경할 경우에는 destrunction (비구조화,
+    // 구조 분해)는 사용할 수 없으니 `by`를 써봅시다.
+    // `remember`는 제거해야 합니다.
+    val (text, setText) = remember { mutableStateOf("") }
+
+    // 단계 5: `toDoList`, `onSubmit`, `onEdit`, `onToggle`,
+    // `onDelete`를 모두 뷰 모델로 옮겨봅시다.
+    val toDoList = remember { mutableStateListOf<ToDoData>() }
+
+    val onSubmit: (String) -> Unit = {
+        val key = (toDoList.lastOrNull()?.key ?: 0) + 1
+        toDoList.add(ToDoData(key, it))
+        setText("")
+    }
+
+    val onEdit: (Int, String) -> Unit = { key, newText ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(text = newText)
+    }
+
+    val onToggle: (Int, Boolean) -> Unit = { key, checked ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(done = checked)
+    }
+
+    val onDelete: (Int) -> Unit = { key ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList.removeAt(i)
+    }
+
+    Scaffold {
+        Column {
+            ToDoInput(
+                text = text,
+                onTextChange = setText,
+                onSubmit = onSubmit
+            )
+            LazyColumn {
+                items(
+                    items = toDoList,
+                    key = { it.key }
+                ) { toDoData ->
+                    ToDo(
+                        toDoData = toDoData,
+                        onEdit = onEdit,
+                        onToggle = onToggle,
+                        onDelete = onDelete
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ViewModelTheme {
+        TopLevel()
+    }
+}
+
+@Composable
+fun ToDoInput(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onSubmit: (String) -> Unit
+) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = onTextChange,
+            modifier = Modifier.weight(1f)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Button(onClick = {
+            onSubmit(text)
+        }) {
+            Text("입력")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoInputPreview() {
+    ViewModelTheme {
+        ToDoInput("테스트", {}, {})
+    }
+}
+
+@Composable
+fun ToDo(
+    toDoData: ToDoData,
+    onEdit: (key: Int, text: String) -> Unit = { _, _ -> },
+    onToggle: (key: Int, checked: Boolean) -> Unit = { _, _ -> },
+    onDelete: (key: Int) -> Unit = {}
+) {
+    var isEditing by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier.padding(4.dp),
+        elevation = 8.dp
+    ) {
+        Crossfade(
+            targetState = isEditing,
+        ) {
+            when (it) {
+                false -> {
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = toDoData.text,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Text("완료")
+                        Checkbox(
+                            checked = toDoData.done,
+                            onCheckedChange = { checked ->
+                                onToggle(toDoData.key, checked)
+                            }
+                        )
+                        Button(
+                            onClick = { isEditing = true }
+                        ) {
+                            Text("수정")
+                        }
+                        Spacer(modifier = Modifier.size(4.dp))
+                        Button(
+                            onClick = { onDelete(toDoData.key) }
+                        ) {
+                            Text("삭제")
+                        }
+                    }
+                }
+                true -> {
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        val (text, setText) = remember { mutableStateOf(toDoData.text) }
+                        OutlinedTextField(
+                            value = text,
+                            onValueChange = setText,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.size(8.dp))
+                        Button(onClick = {
+                            isEditing = false
+                            onEdit(toDoData.key, text)
+                        }) {
+                            Text("완료")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoPreview() {
+    ViewModelTheme {
+        ToDo(ToDoData(1, "nice", true))
+    }
+}
+
+data class ToDoData(
+    val key: Int,
+    val text: String,
+    val done: Boolean = false
+)
